b72201025e0a622c309adef6bded674dc6dc0dac
Giovanni Ap. S. Oliveira <giovanniapsoliveira@gmail.com>
2024-02-26 21:29:28 -0300

    simtask 0.1.0

--- custom/simtask/handle.go
+++ custom/simtask/handle.go
@@ -3,24 +3,21 @@ package function
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
+const Version = "0.1.0"
+
 func Handle(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
-	te0 := time.Now()
+	rt0 := time.Now()
 	params := req.URL.Query()
 	if !params.Has("cl") {
 		resp.WriteHeader(200)
 		_, _ = resp.Write([]byte(""))
-		return
-	}
-
-	rt0 := time.Now().UnixNano()
-	t0, err := strconv.ParseUint(params.Get("t0"), 10, 64)
-	if err != nil {
-		http.Error(resp, "bad 't0' parameter", 400)
 		return
 	}
 	ts, err := strconv.ParseInt(params.Get("ts"), 10, 64)
@@ -53,19 +50,15 @@ func Handle(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 	}
 	rtb := time.Now().Sub(tb0)
 	rts := tb0.Sub(ts0)
-	rte := time.Now().Sub(te0)
+	rtf := time.Now()
+	rdt := rtf.Sub(rt0)
 
 	res := map[string]any{}
-	res["cl"] = params.Get("cl")
-	res["rid"] = params.Get("id")
-	res["fid"] = params.Get("fid")
-	res["t0"] = t0
-	res["rt0"] = rt0
-	res["rtb"] = rtb
-	res["tb"] = tb
-	res["rts"] = rts
-	res["ts"] = ts
-	res["rte"] = rte
+	res["rt0"] = rt0.UnixNano()
+	res["rtb"] = rtb.Nanoseconds()
+	res["rts"] = rts.Nanoseconds()
+	res["rdt"] = rdt.Nanoseconds()
+	res["rtf"] = rtf.UnixNano()
 
 	r, err := json.Marshal(res)
 	if err != nil {
@@ -76,8 +69,12 @@ func Handle(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 			return
 		}
 	}
+	resp.Header().Add("Content-Type", "application/json")
+	resp.Header().Add("X-Request-ID", params.Get("id"))
+	resp.Header().Add("X-Request-Function", strings.Split(req.Host, ".")[0])
+	resp.Header().Add("Version", Version)
 	resp.WriteHeader(200)
-	_, err = resp.Write(r)
+	_, err = fmt.Fprintf(resp, string(r))
 	if err != nil {
 		http.Error(resp, err.Error(), 500)
 		return
