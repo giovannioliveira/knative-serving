ae3c1bc97dc1b0e8891f13d678a32a57b011def9
Giovanni Ap. S. Oliveira <giovanniapsoliveira@gmail.com>
2024-03-06 22:51:05 -0300

    sintask logging more data

--- custom/simtask/handle.go
+++ custom/simtask/handle.go
@@ -4,6 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/load"
+	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/process"
+	"golang.org/x/sys/unix"
 	"net/http"
 	"strconv"
 	"time"
@@ -12,6 +18,8 @@ import (
 const Version = "0.1.1"
 
 func Handle(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	_, _ = cpu.Percent(0, true)
+	_, _ = disk.IOCounters()
 	rt0 := time.Now()
 	params := req.URL.Query()
 	if !params.Has("cl") {
@@ -52,7 +60,7 @@ func Handle(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 	rtf := time.Now()
 	rdt := rtf.Sub(rt0)
 
-	res := map[string]string{}
+	res := map[string]any{}
 	res["rt0"] = strconv.FormatInt(rt0.UnixNano(), 10)
 	res["rtb"] = strconv.FormatInt(rtb.Nanoseconds(), 10)
 	res["rit"] = strconv.FormatInt(rit, 10)
@@ -60,11 +68,82 @@ func Handle(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 	res["rdt"] = strconv.FormatInt(rdt.Nanoseconds(), 10)
 	res["rtf"] = strconv.FormatInt(rtf.UnixNano(), 10)
 
+	times, err := cpu.Times(true)
+	if err == nil {
+		res["cpu_times"] = times
+	}
+	cpupc, err := cpu.Percent(0, true)
+	if err == nil {
+		res["cpu_pc"] = cpupc
+	}
+	iouse, err := disk.IOCounters("/dev/xvda1", "/dev/xvda2", "/dev/sda") //sda for local tests
+	if err == nil {
+		res["iouse"] = iouse
+	}
+	memstat, err := mem.VirtualMemory()
+	if err == nil {
+		res["memstat"] = memstat
+	}
+	memexstat, err := mem.VirtualMemory()
+	if err == nil {
+		res["memexstat"] = memexstat
+	}
+	avgstat, err := load.Avg()
+	if err == nil {
+		res["load"] = avgstat
+	}
+	miscstat, err := load.Avg()
+	if err == nil {
+		res["miscstat"] = miscstat
+	}
+	proc, err := process.NewProcess(int32(unix.Getpid()))
+	psconn, err := proc.Connections()
+	if err == nil {
+		res["psconn"] = psconn
+	}
+	psio, err := proc.IOCounters()
+	if err == nil {
+		res["psio"] = psio
+	}
+	psmem, err := proc.MemoryInfo()
+	if err == nil {
+		res["psmem"] = psmem
+	}
+	pstimes, err := proc.Times()
+	if err == nil {
+		res["pstimes"] = pstimes
+	}
+	pscpupc, err := proc.CPUPercent()
+	if err == nil {
+		res["pscpupc"] = pscpupc
+	}
+	psmempc, err := proc.MemoryPercent()
+	if err == nil {
+		res["psmempc"] = psmempc
+	}
+	pccreatets, err := proc.CreateTime()
+	if err == nil {
+		res["pccreatets"] = pccreatets
+	}
+	psctxsw, err := proc.NumCtxSwitches()
+	if err == nil {
+		res["psctxsw"] = psctxsw
+	}
+	psnfd, err := proc.NumFDs()
+	if err == nil {
+		res["psnfd"] = psnfd
+	}
+	psth, err := proc.Threads()
+	if err == nil {
+		res["psth"] = psth
+	}
+
 	r, err := json.Marshal(res)
 	if err != nil {
 		http.Error(resp, err.Error(), 500)
 		return
 	}
+
 	resp.Header().Add("Content-Type", "plain/text")
 	resp.Header().Add("X-Request-ID", params.Get("id"))
 	resp.Header().Add("Version", Version)
